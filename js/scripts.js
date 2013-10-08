fa13b9856241d1b58e41ce8eea0ee0e6d6276af1
Fuggle <whoiscatnow@gmail.com>
2013-10-08 20:12:26 +1000

    javascript random colours script

--- js/scripts.js
+++ js/scripts.js
@@ -40,5 +40,45 @@ $(document).ready(function(){
 	//colour pattern is always  > melon > yellow > green > purple > blue > teal >
 	//but start point can be any colour.
 
+	var h2count = $("h2, h3").length - 1;
+	var start_point =  Math.ceil(Math.random() * h2count);
+	$("h2, h3").removeClass();
+	colourHeadings(start_point, h2count);
 
 });
+
+	//called on the start point, and hand out classes in order until it runs out of h2 and h3 elements
+	function colourHeadings(s, c){
+
+		var pageCount = s;
+		var colours = ["blue","teal","melon","yellow","green","purple"];
+		var arrayCount = 1;
+
+		for(var i = 0; i <= c; i++){
+			console.log("pagecount: " +pageCount + " ---- arrayCount: " + arrayCount + "   ---ind: " + i);
+			if (i == 0) {
+				$("h2, h3").eq(s).addClass(colours[i]);
+			} else {
+				$("h2, h3").eq(pageCount).addClass(colours[arrayCount]);
+			};
+
+			if (pageCount == c) {
+				pageCount = 0;
+			} else { 
+				pageCount = pageCount + 1;
+			};
+
+			if (arrayCount == colours.length - 1) {
+				arrayCount = 1;
+			} else {
+				arrayCount = arrayCount + 1;
+			};
+
+		}
+	}
+
+
+
+
+
+
